e166664d3c650eda4726b8d6e520beea01339e50
Aleksey <aleksey91scorp@bk.ru>
2020-07-15 09:21:12 +0300

    basic geolocation

---
+++ src/SharePlace.js
@@ -0,0 +1,36 @@
+class PlaceFinder {
+  constructor() {
+    const addressForm = document.querySelector("form");
+    const locateUserBtn = document.getElementById("locate-btn");
+
+    locateUserBtn.addEventListener("click", this.locateUserHandler);
+    addressForm.addEventListener("submit", this.findAddressHandler);
+  }
+
+  locateUserHandler() {
+    if (!navigator.geolocation) {
+      alert(
+        "Location feature is not available in your browser - please use a more modern browser or manually enter an address."
+      );
+      return;
+    }
+    navigator.geolocation.getCurrentPosition(
+      (successResult) => {
+        const coordinates = {
+          lat: successResult.coords.latitude,
+          lng: successResult.coords.longitude,
+        };
+        console.log(coordinates);
+      },
+      (error) => {
+        alert(
+          "Could not locate you unfortunately. Please enter an address manually!"
+        );
+      }
+    );
+  }
+
+  findAddressHandler() {}
+}
+
+const placeFinder = new PlaceFinder();
