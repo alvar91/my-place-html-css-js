044ff92fbe2561bdeba5fbfac93ebba734462771
Aleksey <aleksey91scorp@bk.ru>
2020-07-15 10:06:02 +0300

    show modal with spinner

--- src/SharePlace.js
+++ src/SharePlace.js
@@ -1,3 +1,5 @@
+import { Modal } from "./UI/Modal";
+
 class PlaceFinder {
   constructor() {
     const addressForm = document.querySelector("form");
@@ -14,8 +16,14 @@ class PlaceFinder {
       );
       return;
     }
+    const modal = new Modal(
+      "loading-modal-content",
+      "Loading location - please wait!"
+    );
+    modal.show();
     navigator.geolocation.getCurrentPosition(
       (successResult) => {
+        modal.hide();
         const coordinates = {
           lat: successResult.coords.latitude,
           lng: successResult.coords.longitude,
@@ -23,6 +31,7 @@ class PlaceFinder {
         console.log(coordinates);
       },
       (error) => {
+        modal.hide();
         alert(
           "Could not locate you unfortunately. Please enter an address manually!"
         );
